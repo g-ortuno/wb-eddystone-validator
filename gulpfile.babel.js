67e732547258311c5294b5244e07c68e48b7a851
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-16 18:39:22 -0800

    Add jshint for correctness testing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,13 @@
-import gulp from 'gulp'
+import gulp from 'gulp';
+import jshint from 'gulp-jshint';
 
-gulp.task('stub', () => {
-  console.log('Stub Task');
+gulp.task('test', [
+  'test:jshint'
+]);
+
+gulp.task('test:jshint', () => {
+  return gulp.src(['gulpfile.babel.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
