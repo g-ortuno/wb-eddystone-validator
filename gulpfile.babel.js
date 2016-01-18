80d1a936e431326aa4e7721eeba05355fdf3a364
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-17 16:00:38 -0800

    Add style checker

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 import gulp from 'gulp';
 import jshint from 'gulp-jshint';
+import jscs from 'gulp-jscs';
 
 gulp.task('test', [
-  'test:jshint'
+  'test:jshint',
+  'test:jscs'
 ]);
 
 gulp.task('test:jshint', () => {
@@ -10,4 +12,10 @@ gulp.task('test:jshint', () => {
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test:jscs', () => {
+  return gulp.src(['gulpfile.babel.js'])
+    .pipe(jscs())
+    .pipe(jscs.reporter());
 });
