08ffcd16c2173426b1778b7a12855567e57a2b55
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-18 18:56:07 -0800

    Add chai and chai as promised for assertions

--- tests.js
+++ tests.js
@@ -1,9 +1,14 @@
 (() => {
   'use strict';
+  let expect = chai.expect;
+
+  let stub_func = () => {
+    return new Promise(resolve => setTimeout(() => resolve('Stub'), 1500));
+  };
 
   describe('Stub suite', () => {
     it('Stub test', () => {
-      console.log('Stub test running...');
+      return expect(stub_func()).to.eventually.equal('Stub');
     });
   });
 })();
