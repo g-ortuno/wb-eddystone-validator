77979a983bc31dace7a8e6f78f4b714c189ee2ff
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-19 18:57:57 -0800

    Add new test suite

--- setup.js
+++ setup.js
@@ -7,4 +7,8 @@
     window.history.pushState({}, 'Test', '/?grep=Core Eddystone-URL Tests');
     mocha.run();
   };
+  window.startServicesDiscoveredTest = () => {
+    window.history.pushState({}, 'Test', '/?grep=Services Discovered');
+    mocha.run();
+  };
 })();
