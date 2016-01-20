d58a96363b59b25fa36bbf4b2b5d1f0bdd8bee4e
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-19 19:32:56 -0800

    Clear previous test status

--- setup.js
+++ setup.js
@@ -1,13 +1,20 @@
 (() => {
   'use strict';
 
+  let clearPreviousTests = () => {
+    let testDiv = document.querySelector('#mocha');
+    testDiv.innerHTML = "";
+  };
+
   mocha.setup('bdd');
 
   window.startCoreTest = () => {
+    clearPreviousTests();
     window.history.pushState({}, 'Test', '/?grep=Core Eddystone-URL Tests');
     mocha.run();
   };
   window.startServicesDiscoveredTest = () => {
+    clearPreviousTests();
     window.history.pushState({}, 'Test', '/?grep=Services Discovered');
     mocha.run();
   };
