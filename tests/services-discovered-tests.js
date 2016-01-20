9466588f712a22f6fc2900db315c44f1e21fc90b
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-19 19:31:27 -0800

    Add test for time it takes to discover the services

--- tests/services-discovered-tests.js
+++ tests/services-discovered-tests.js
@@ -2,8 +2,34 @@
   'use strict';
   let expect = chai.expect;
 
-  describe('Services Discovered Tests', () => {
-    it('Stub', () => {
+  const CONFIG_UUID = 'ee0c2080-8786-40ba-ab96-99b91ac981d8';
+  let global_device;
+  let global_server;
+
+  describe('Services Discovered Tests', function() {
+    // Set slow to 0 so that we see the times for all tests.
+    this.slow(0);
+    before(function(done) {
+      this.timeout(0);
+      if (!global_device) {
+        navigator.bluetooth
+          .requestDevice({filters: [{services: [CONFIG_UUID]}]}).then(device => {
+            global_device = device;
+            done();
+          });
+      } else {
+        done();
+      }
+    });
+    afterEach(() => {
+      // TODO(g-ortuno): Add disconnect after each test.
+    });
+    it('Services Discovered Test', function() {
+      this.timeout(0);
+      return expect(global_device.connectGATT().then(g => {
+        global_server = g;
+        g.getPrimaryService(CONFIG_UUID);
+      })).to.be.fulfilled;
     });
   });
 })();
