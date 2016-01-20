a7aa348c06de7f07bba25b02d429457aea85aabc
Giovanni Ortuño <gd.ortuno@gmail.com>
2016-01-19 20:01:46 -0800

    Separate core tests

--- tests/core-eddystone-tests.js
+++ tests/core-eddystone-tests.js
@@ -64,38 +64,64 @@
       // why we get a GATT operation in progress error.
       setTimeout(done, 1000);
     });
-    it('Read Lock State', () => {
-      return expect(lockState().readValue().then(toArray))
-        .to.eventually.deep.equal([0]);
+    describe('Lock State', () => {
+      it('Read Lock State', () => {
+        return expect(lockState().readValue().then(toArray))
+          .to.eventually.deep.equal([0]);
+      });
     });
-    it('Write Reset', () => {
-      return expect(reset().writeValue(new Uint8Array([1])))
+    describe('Reset', () => {
+      it('Write Reset', () => {
+        return expect(reset().writeValue(new Uint8Array([1])))
           .to.be.fulfilled;
+      });
     });
-    it('Write and Read Data', () => {
-      return expect(data().writeValue(new Uint8Array([0,0]))
-          .then(() => data().readValue())
-          .then(toArray)).to.eventually.deep.equal([0,0]);
+    describe('Data', () => {
+      it('Write Data', () => {
+        return expect(data().writeValue(new Uint8Array([0,0])))
+          .to.be.fulfilled;
+      });
+      it('Read Data', () => {
+        return expect(data().readValue().then(toArray))
+          .to.eventually.deep.equal([0, 0]);
+      });
     });
-    it('Write and Read Tx Power Levels', () => {
+    describe('Tx Power Levels', () => {
       const TX_POWER_LEVELS = [1, 2, 3, 4];
-      return expect(powerLevels().writeValue(new Uint8Array(TX_POWER_LEVELS))
-          .then(() => powerLevels().readValue())
-          .then(toArray)).to.eventually.deep.equal(TX_POWER_LEVELS);
-    });
-    it('Write and Read Tx Power Mode', () => {
-      return expect(powerMode().writeValue(new Uint8Array([0]))
-          .then(() => powerMode().readValue())
-          .then(toArray)).to.eventually.deep.equal([0]);
-    });
-    it('Write and read Period', () => {
-      return expect(period().writeValue(new Uint16Array([1000]))
-          .then(() => period().readValue())
-          .then(toArray16)).to.eventually.deep.equal([1000]);
-    });
-    it('Write Reset', () => {
-      return expect(reset().writeValue(new Uint8Array([1])))
+      it('Write Levels', () => {
+        return expect(powerLevels().writeValue(new Uint8Array(TX_POWER_LEVELS)))
           .to.be.fulfilled;
+      });
+      it('Read Levels', () => {
+        return expect(powerLevels().readValue().then(toArray))
+          .to.eventually.deep.equal(TX_POWER_LEVELS);
+      });
+    });
+    describe('Tx Power Mode', () => {
+      it('Write Mode', () => {
+        return expect(powerMode().writeValue(new Uint8Array([0])))
+          .to.be.fulfilled;
+      });
+      it('Read Mode', () => {
+        return expect(powerMode().readValue().then(toArray))
+          .to.eventually.deep.equal([0]);
+      });
+    });
+    describe('Period', () => {
+      it('Write Period', () => {
+        return expect(period().writeValue(new Uint16Array([1000])))
+          .to.be.fulfilled;
+      });
+      it('Write and read Period', () => {
+        return expect(period().readValue().then(toArray16))
+          .to.eventually.deep.equal([1000]);
+      });
+    });
+    describe('Reset to defaults', () => {
+      it('Write Reset', () => {
+        return expect(reset().writeValue(new Uint8Array([1])))
+          .to.be.fulfilled;
+      });
     });
   });
 })();
