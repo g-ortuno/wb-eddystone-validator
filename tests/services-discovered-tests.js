18edce455ebf4ff449ff4b2ab4925aa16afa8509
Giovanni Ortuño Urquidi <gd.ortuno@gmail.com>
2016-03-04 10:20:54 -0800

    Print characteristics

--- tests/services-discovered-tests.js
+++ tests/services-discovered-tests.js
@@ -30,5 +30,42 @@
       this.timeout(0);
       return expect(global_device.gatt.getPrimaryService(TEST_UUID)).to.be.fulfilled;
     });
+    it('Print characteristics', function() {
+      this.timeout(0);
+      return expect(global_device.gatt.getPrimaryService(TEST_UUID)
+          .then(s => {
+            s.getCharacteristics()
+                .then(characteristics => {
+                  let output_div = document.querySelector('div#test-output')
+
+                  let service_heading_text = document.createTextNode('Service:');
+                  let service_heading = document.createElement('h3');
+                  service_heading.appendChild(service_heading_text);
+                  output_div.appendChild(service_heading);
+
+                  let service_uuid = document.createTextNode(s.uuid);
+                  output_div.appendChild(service_uuid);
+
+                  let characteristics_heading_text = document.createTextNode(
+                      'Characteristics:');
+                  let characteristics_heading = document.createElement('h4');
+                  characteristics_heading.appendChild(
+                      characteristics_heading_text);
+
+                  output_div.appendChild(characteristics_heading);
+
+                  let characteristic_count = 1;
+                  characteristics.forEach(characteristic => {
+                    let text = document.createTextNode(
+                            characteristic_count++ + ". " + characteristic.uuid);
+
+                    let char_div = document.createElement('div');
+                    char_div.appendChild(text);
+
+                    output_div.appendChild(char_div);
+                  });
+                });
+          })).to.be.fulfilled;
+    });
   });
 })();
